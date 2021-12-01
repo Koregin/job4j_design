23d9c039543df6066b32e9f5b40afdd5673c9c72
Evgeny Koregin <mnemonic78@yandex.ru>
2021-12-01 12:23:20 +0300

    0.4. BufferedOutputStream [#252490]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,15 +1,20 @@
 package ru.job4j.io;
 
+import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
+import java.io.PrintWriter;
 
 public class ResultFile {
     public static void main(String[] args) {
-        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream("result.txt")
+                ))) {
             for (int row = 1; row < 10; row++) {
                 for (int col = 1; col < 10; col++) {
-                    out.write(new String(row * col + "|").getBytes());
+                    out.printf("%d%s", row * col, "|");
                 }
-                out.write(System.lineSeparator().getBytes());
+                out.println();
             }
         } catch (Exception e) {
             e.printStackTrace();
