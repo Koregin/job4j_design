49af633d1bdd3aa31619fc231f1661f14f13f498
Evgeny Koregin <mnemonic78@yandex.ru>
2021-12-02 16:41:44 +0300

    3.0. Тестирование IO [#173905]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -3,14 +3,14 @@ package ru.job4j.io;
 import java.io.*;
 
 public class Analizy {
-    public void unavailable(String source, String target) {
+    public static void unavailable(String source, String target) {
         try (BufferedReader read = new BufferedReader(new FileReader(source));
         PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
             String line;
             StringBuilder sb = new StringBuilder();
             boolean unavailable = false;
             while ((line = read.readLine()) != null) {
-                String[] pair = line.split(" ");
+                String[] pair = line.split("\\s+");
                 if (!unavailable && (pair[0].equals("400") || pair[0].equals("500"))) {
                     unavailable = true;
                     sb.append(pair[1]).append(";");
@@ -25,16 +25,5 @@ public class Analizy {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public static void main(String[] args) {
-        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
-            out.println("15:01:30;15:02:32");
-            out.println("15:10:30;23:12:32");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        new Analizy().unavailable("./data/sourcelog1.log", "unavailable1.csv");
-        new Analizy().unavailable("./data/sourcelog2.log", "unavailable2.csv");
     }
 }
