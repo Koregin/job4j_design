a7437b42716871fa779319428b0da209aa1b8c66
Evgeny Koregin <mnemonic78@yandex.ru>
2021-12-04 15:43:42 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -2,6 +2,7 @@ package ru.job4j.io;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -9,8 +10,15 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith("js")).forEach(System.out::println);
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Not correct arguments. Usage: java -jar START_DIR FILE_EXT");
+        }
+        Path start = Paths.get(args[0]);
+        try {
+            search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+        } catch (NoSuchFileException exc) {
+            System.out.println("Searching path isn't exist");
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
