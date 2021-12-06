e52d1459f29bba311cabe99b592a9fc0d05ae045
Evgeny Koregin <mnemonic78@yandex.ru>
2021-12-06 18:36:12 +0300

    6. Кодировка. [#862]

---
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -0,0 +1,79 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConsoleChat {
+    private final String path;
+    private final String botAnswers;
+    private static final String OUT = "закончить";
+    private static final String STOP = "стоп";
+    private static final String CONTINUE = "продолжить";
+
+    public ConsoleChat(String path, String botAnswers) {
+        this.path = path;
+        this.botAnswers = botAnswers;
+    }
+
+    public void run() {
+        List<String> botAnswers = readPhrases();
+        String userAnswer = "";
+        boolean stopAction = false;
+        List<String> forLog = new ArrayList<>();
+        BufferedReader bufRead = new BufferedReader(
+                new InputStreamReader(System.in));
+        System.out.println("Начинаем чат:");
+        while (!OUT.equals(userAnswer)) {
+            try {
+                userAnswer = bufRead.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (STOP.equals(userAnswer)) {
+                stopAction = true;
+            } else if (CONTINUE.equals(userAnswer)) {
+                stopAction = false;
+            }
+            if (!stopAction) {
+                String botAnswer = botAnswers.get((int) (Math.random() * botAnswers.size()));
+                System.out.println(botAnswer);
+                forLog.add(botAnswer);
+            }
+            forLog.add(userAnswer);
+        }
+        saveLog(forLog);
+        System.out.println("До свидания!");
+    }
+
+    private List<String> readPhrases() {
+        List<String> phrasesList = new ArrayList<>();
+        String phrase = "";
+        try (BufferedReader botAns =
+                new BufferedReader(new FileReader(botAnswers))) {
+            while ((phrase = botAns.readLine()) != null) {
+                phrasesList.add(phrase);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return phrasesList;
+    }
+
+    private void saveLog(List<String> log) {
+        try (BufferedWriter bufLogs =
+                new BufferedWriter(new FileWriter(path))) {
+            for (String line : log) {
+                bufLogs.write(line);
+                bufLogs.newLine();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ConsoleChat cc = new ConsoleChat("./data/consoleChat.log", "./data/botAnswers.txt");
+        cc.run();
+    }
+}
