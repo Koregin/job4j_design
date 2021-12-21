70ab11044038ba12373d271054107bb1ea3243ba
Evgeny Koregin <mnemonic78@yandex.ru>
2021-12-21 16:41:50 +0300

    0.1. Statement [#379306 #242483]

--- src/main/java/ru/job4j/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -24,39 +24,39 @@ public class TableEditor implements AutoCloseable {
         connection = DriverManager.getConnection(url, login, password);
     }
 
+    private Statement createStatement() throws SQLException {
+        return connection.createStatement();
+    }
+
     public void createTable(String tableName) throws SQLException {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format("create table if not exists %s()", tableName);
-            statement.execute(sql);
-        }
+        String sql = String.format("create table if not exists %s()", tableName);
+        createStatement().execute(sql);
+        createStatement().close();
+
     }
 
     public void dropTable(String tableName) throws SQLException {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format("drop table if exists %s", tableName);
-            statement.execute(sql);
-        }
+        String sql = String.format("drop table if exists %s", tableName);
+        createStatement().execute(sql);
+        createStatement().close();
     }
 
     public void addColumn(String tableName, String columnName, String type) throws SQLException {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format("alter table %s add column if not exists %s %s", tableName, columnName, type);
-            statement.execute(sql);
-        }
+        String sql = String.format("alter table %s add column if not exists %s %s", tableName, columnName, type);
+        createStatement().execute(sql);
+        createStatement().close();
     }
 
     public void dropColumn(String tableName, String columnName) throws SQLException {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format("alter table %s drop column if exists %s", tableName, columnName);
-            statement.execute(sql);
-        }
+        String sql = String.format("alter table %s drop column if exists %s", tableName, columnName);
+        createStatement().execute(sql);
+        createStatement().close();
     }
 
     public void renameColumn(String tableName, String columnName, String newColumnName) throws SQLException {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format("alter table %s rename %s to %s", tableName, columnName, newColumnName);
-            statement.execute(sql);
-        }
+        String sql = String.format("alter table %s rename %s to %s", tableName, columnName, newColumnName);
+        createStatement().execute(sql);
+        createStatement().close();
     }
 
     public static String getTableScheme(Connection connection, String tableName) throws Exception {
