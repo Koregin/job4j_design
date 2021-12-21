e047e816b6c558b408323b7cd32baa144d81c642
Evgeny Koregin <mnemonic78@yandex.ru>
2021-12-21 17:02:30 +0300

    0.1. Statement [#379306 #242483]

--- src/main/java/ru/job4j/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -24,39 +24,35 @@ public class TableEditor implements AutoCloseable {
         connection = DriverManager.getConnection(url, login, password);
     }
 
-    private Statement createStatement() throws SQLException {
-        return connection.createStatement();
+    private void createStatement(String sql) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
     }
 
     public void createTable(String tableName) throws SQLException {
         String sql = String.format("create table if not exists %s()", tableName);
-        createStatement().execute(sql);
-        createStatement().close();
-
+        createStatement(sql);
     }
 
     public void dropTable(String tableName) throws SQLException {
         String sql = String.format("drop table if exists %s", tableName);
-        createStatement().execute(sql);
-        createStatement().close();
+        createStatement(sql);
     }
 
     public void addColumn(String tableName, String columnName, String type) throws SQLException {
         String sql = String.format("alter table %s add column if not exists %s %s", tableName, columnName, type);
-        createStatement().execute(sql);
-        createStatement().close();
+        createStatement(sql);
     }
 
     public void dropColumn(String tableName, String columnName) throws SQLException {
         String sql = String.format("alter table %s drop column if exists %s", tableName, columnName);
-        createStatement().execute(sql);
-        createStatement().close();
+        createStatement(sql);
     }
 
     public void renameColumn(String tableName, String columnName, String newColumnName) throws SQLException {
         String sql = String.format("alter table %s rename %s to %s", tableName, columnName, newColumnName);
-        createStatement().execute(sql);
-        createStatement().close();
+        createStatement(sql);
     }
 
     public static String getTableScheme(Connection connection, String tableName) throws Exception {
